008f4b6839a67c2d83a3b211af0116a1d08429d9
Meghana A <maggiemegh243@gmail.com>
2026-07-10 23:07:37 +0530

    feat: add task API endpoints

--- create_table.py
+++
@@ -1,7 +0,0 @@
-# create_tables.py
-from postgre import Base, engine
-import models  # important: ensures Book is registered
-
-
-Base.metadata.create_all(bind=engine)
-print("Tables created!")
--- test_db.py
+++
@@ -1,9 +0,0 @@
-from crypto import hash_password
-
-from database import SessionLocal
-from models import Book
-
-db = SessionLocal()
-db.add(Book(title="The Great Gatsby", author="F. Scott Fitzgerald",hashed_password=hash_password("secret123")))
-db.commit()
-db.close()
